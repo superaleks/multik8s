c69e1a603cc042e0f3736d7e360010a013935527
Aleksandra Lukic <aleksandralukic@Aleksandras-Air.home>
2021-05-28 15:15:47 +0200

    A typo

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1> Aleks made a change: KUBERNETES </h>
+          <h1> Aleks made a change: KUBERNETES </h1>
           <a
             className="App-link"
             href="https://reactjs.org"
